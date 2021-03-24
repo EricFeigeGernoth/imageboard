4fcb5f00e3c42804541fd36c39e37bf06b254158
Eric Feige-Gernoth <ericfg@protonmail.com>
2021-03-24 13:20:07 +0100

    update

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,10 @@
         data: {
             name: "Eric",
             images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null,
             // this is the perfect location for us to ask if there are any images to retrieve any images from our database
         },
         mounted: function () {
@@ -15,16 +19,53 @@
                     console.log("resp from /images: ", resp);
                     self.images = resp.data;
                 })
-                .catch("err: ", err);
+                .catch((err) => console.log("err", err));
         },
 
         methods: {
-            myFunction: function (arg) {
-                console.log(
-                    "myFn is running & the argument passed to it is: ",
-                    arg
-                );
+            handleClick: function (e) {
+                // e.preventDefault();
+                console.log("click on button");
+
+                console.log(this.title);
+                console.log(this.description);
+
+                console.log(this.username);
+                console.log(this.file);
+                // we use FormData to send files over to the server!
+                var formData = new FormData();
+                //the file MUST go in formData
+                //but I'm putting them here for convenience
+                // ie it's nice to put all of the data we're sending over to the server
+                // in one place
+                formData.append("file", this.file);
+                formData.append("description", this.description);
+                formData.append("title", this.title);
+                formData.append("username", this.username);
+
+                console.log("formData: ", formData);
+                axios
+                    .post("/upload", formData)
+                    .then((resp) => {
+                        console.log("resp from POST /upload: ", resp);
+                    })
+                    .catch((err) => {
+                        console.log("err in POST /upload: ", err);
+                    });
             },
+
+            handleChange: function (e) {
+                console.log("handleChange is running");
+                console.log("e.target: ", e.target.files);
+                this.file = e.target.files[0];
+            },
+
+            // myFunction: function (arg) {
+            //     console.log(
+            //         "myFn is running & the argument passed to it is: ",
+            //         arg
+            //     );
+            // },
         },
     });
 })();
--- server.js
+++ server.js
@@ -4,7 +4,49 @@ const app = express();
 
 const { getImages } = require("./db.js");
 
+//middleware
 app.use(express.static("./public"));
+
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+// be mindful that images that are over 2mb will not be uploaded! so if you try to upload an
+// image and it doesn't work - it might be the image is too big :)
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("Working");
+    console.log("req.body: ", req.body);
+    console.log("req.file: ", req.file);
+    if (req.file) {
+        // this runs if everything worked!
+        res.json({
+            success: true,
+        });
+    } else {
+        // this runs if something goes wrong along the way :(
+        res.json({
+            success: false,
+        });
+    }
+});
 
 app.get("/images", (req, res) => {
     console.log("images has been hit");
